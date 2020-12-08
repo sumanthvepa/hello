947f2b8d140768592302bead921d90c21f763b55
Sumanth Vepa <svepa@svertual.in>
2020-12-08 13:16:33 +0530

    [issue001] Create the HelloWorld Project using Xcode
    FEATURE(s)
    issue001-macos-command-line-xcode-cplusplus-hello:
      Hello World command line program for macOS built using Xcode
    
    FILE(s)
    [new].gitignore: Ignore macOS artifacts
    [modified]README:
      Add MacOS | Command Line Program | XCode | C++ version of
      the 'Hello World' program
    [new]TODO.txt
      Add issue001-macos-command-line-xcode-cplusplus-hello
    [new]macos/cplusplus/console/xcode/hello/.gitignore
      Ignore Xcode artifacts
    [new]macos/cplusplus/console/xcode/hello/hello.xcodeproj/project.pbxproj
      Xcode project artifact
    [new]macos/cplusplus/console/xcode/hello/hello.xcodeproj/project.xcworkspace/contents.xcworkspacedata
      Xcode project artifact
    [new]macos/cplusplus/console/xcode/hello/hello.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist
      Xcode project artifacts
    [new]macos/cplusplus/console/xcode/hello/hello/main.cpp
      Autogenerated C++ source code file.

---
+++ macos/cplusplus/console/xcode/hello/hello/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Sumanth Vepa on 08/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
