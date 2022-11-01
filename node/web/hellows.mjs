5b7cc22b57c12b48c33838d37c44b9ae70e6514e
Sumanth Vepa <svepa@milestone42.com>
2022-11-01 16:19:30 +0530

    [issue035] Hello World using pure nodejs (no additional frameworks)
    FEATURE(s)
    issue035-node-web: Hello World using pure nodejs (no additional frameworks)
    
    FILE(s)
    [modified]TODO.txt Add issue035
    [new]node/web/hellows.mjs: The webservice that returns hello world as plain text
    [new]node/web/package.json: NodeJS package specification file
    
    DESCRIPTION
    Hello World using pure nodejs (no additional frameworks)

---
+++ node/web/hellows.mjs
@@ -0,0 +1,20 @@
+"use strict";
+
+import * as os from 'os';
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+const address = '0.0.0.0';
+const port = 5001;
+
+
+
+server.listen(port, address, () => {
+  // TODO: Sent message to log instead of console
+  console.log(`HelloWS running at http://${os.hostname()}(${address}):${port}/`)
+});
